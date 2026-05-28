415e73b9db6e5ede9b943f86f89703201842927a
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-05-28 19:15:54 +0700

    change name

--- orchestration/crawl_fs_financial_statistics_overview_type_four.py
+++ orchestration/crawl_fs_financial_statistics_overview_type_four.py
@@ -10,7 +10,7 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
-    tags=['Crawl Financial Statistics Overview Type Four', 'Product Company', 'Financial Statistics'],   
+    tags=['Crawl Financial Statistics Overview Type Four', 'Product Company', 'Financial Statistics', 'Financial Statement'],   
 ) as dag:
     # Task 1: Bash execution
     start_crawl_dag = BashOperator(
